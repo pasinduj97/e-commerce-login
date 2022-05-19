0f4374d6b3eabb35e96f39fc2ea2b8d79980bff4
pasinduj97 <pasinduj9@gmail.com>
2022-05-19 17:31:29 +0530

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <div class="w-100 d-flex align-items-center justify-content-center">
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email</Form.Label>
+           
             <Form.Control type="email" placeholder="Enter email" />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
